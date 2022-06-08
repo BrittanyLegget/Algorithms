593a899acfe62473fc6cfb30e3862c6a9de2c950
BrittanyLegget <britt.legget@gmail.com>
2022-06-08 14:35:41 -0700

    Update prims

--- Graphs/Prims/prims.py
+++ Graphs/Prims/prims.py
@@ -1,36 +1,35 @@
 import sys
 
 
-class Graph:
-    def prims(G):
-        V = len(G)
-        # Set to hold results
-        result = set()
-        # Track visited nodes and set the first one to true
-        visited = [0 for i in range(V)]
-        visited[0] = True
-        seen = 0
-        while (seen < V - 1):
-            # Initialize minimum to some large number
-            minimum = sys.maxsize
-            x = y = 0
-            for i in range(V):
-                if visited[i]:
-                    for j in range(V):
-                        if ((not visited[j]) and G[i][j]):
-                            # not in visited and there is an edge
-                            if minimum > G[i][j]:
-                                minimum = G[i][j]
-                                x = i
-                                y = j
-            result.add((x, y, G[x][y]))
-            visited[y] = True
-            seen += 1
-        return result
+def prims(G):
+    V = len(G)
+    # Set to hold results
+    result = set()
+    # Track visited nodes and set the first one to true
+    visited = [0 for i in range(V)]
+    visited[0] = True
+    seen = 0
+    while (seen < V - 1):
+        # Initialize minimum to some large number
+        minimum = sys.maxsize
+        x = y = 0
+        for i in range(V):
+            if visited[i]:
+                for j in range(V):
+                    if ((not visited[j]) and G[i][j]):
+                        # not in visited and there is an edge
+                        if minimum > G[i][j]:
+                            minimum = G[i][j]
+                            x = i
+                            y = j
+        result.add((x, y, G[x][y]))
+        visited[y] = True
+        seen += 1
+    return result
 
 
 # Example Graph
-P = Graph
+
 G = [
     [0, 8, 5, 0, 0, 0, 0],
     [8, 0, 10, 2, 18, 0, 0],
@@ -41,4 +40,4 @@ G = [
     [0, 0, 0, 14, 4, 26, 0]
 ]
 
-print(P.prims(G))
+print(prims(G))
